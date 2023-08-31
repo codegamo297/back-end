8c5f66f1f50f5dbabf84f4b5464ddf0374c0122a
Đỗ Tất Hiếu <dotathieu0729@gmail.com>
2023-08-31 22:30:09 +0700

    7. Upload file

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const userRoute = require('./routes/users');
 const authorRoute = require('./routes/author');
 const postRoute = require('./routes/posts');
 const db = require('./config/db');
+const multer = require('multer');
+const path = require('path');
 
 // Connect to db
 db.connect();
@@ -13,10 +15,29 @@ db.connect();
 const app = express();
 const port = 8800;
 
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
 // middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    try {
+        return res.status(200).json('File uploaded successfully!');
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 // Routes init
 app.use('/api/users', userRoute);
